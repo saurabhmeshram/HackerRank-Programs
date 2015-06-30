6049e726b09f76f92ce71f8225b3b2081521cd6c
[Saurabh Meshram] <[saurabh.meshram@globallogic.com]>
2015-06-30 16:24:44 +0530

    Initial commit

---
+++ 1.c
@@ -0,0 +1,27 @@
+/*
+	Exercise Program on HackerRank - Dynamic Programming
+	Link : https://www.hackerrank.com/challenges/maxsubarray	
+*/
+
+#include <stdio.h>
+#define MAX 10
+
+int main(void)
+{
+	int arr[MAX] = {0};
+	int i, arr_total = 0;
+
+	/* Hold values for the two Output */
+	int max1 = 0, max2 = 0;
+	
+	for (i=0; i< MAX-1; i++)
+		scanf("%d", &a[i]);
+
+
+	for (i=0; i< MAX-1; i++)
+		arr_total += a[i];
+
+	/* Assign sum of array as Maximum first */
+	max2 = arr_total;
+	return 0;
+}
